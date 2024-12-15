dc8dd1c0a1854a459feb98f1ae5200664efd178f
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2024-12-15 08:11:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.py
@@ -0,0 +1,45 @@
+class Solution:   
+    def peakElement(self, arr):
+        s, e = 0, len(arr) - 1
+        while s < e:
+            m = (s + e) // 2
+            if arr[m] > arr[m+1]:
+                e = m
+            else:
+                s = m+1
+        return s
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())  # Read number of test cases
+    for _ in range(t):
+        # Read input and split it into a list of integers
+        arr = list(map(int, input().split()))
+        # Create a Solution object and calculate the result
+
+        index = Solution().peakElement(arr)
+        n = len(arr)
+        flag = False
+        if index < 0 or index >= n:
+            flag = False
+        else:
+            if index == 0 and n == 1:
+                flag = True
+            elif index == 0 and arr[index] > arr[index + 1]:
+                flag = True
+            elif index == n - 1 and arr[index] > arr[index - 1]:
+                flag = True
+            elif index > 0 and index < n - 1 and arr[
+                    index - 1] < arr[index] and arr[index] > arr[index + 1]:
+                flag = True
+            else:
+                flag = False
+
+        if flag:
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
