8927e261474804bdb55b9a699bce60017843bb6c
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-07-08 09:26:47 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findGreater(self, arr):
+        # code here
+        
+        from collections import Counter
+        cnt=Counter(arr)
+        ret=[-1]*len(arr)
+        stk=[]
+        for ix,ve in enumerate(arr):
+            while stk and cnt[arr[stk[-1]]]<cnt[ve]:
+                ret[stk[-1]]=ve
+                stk.pop()
+            stk.append(ix)
+        return ret
+
