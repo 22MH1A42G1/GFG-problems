acc5418060207d0377f29dc07cd1e21e9b404451
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-10-23 23:27:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.py
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.py
@@ -1,39 +1,10 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-from typing import List
-
-
-# } Driver Code Ends
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Your code here
-        def fun(x):
-            return x[0]**2 + x[1]**2 
-        points.sort(key=lambda x: fun(x))
-        #print(points)
-        return points[:k]
-        
-
-#{ 
- # Driver Code Starts.
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        t -= 1
-        k = int(input())
-        n = int(input())
-        points = []
-        for _ in range(n):
-            x, y = map(int, input().split())
-            points.append([x, y])
-        
-        solution = Solution()
-        ans = solution.kClosest(points, k)
-        ans.sort()
-        for point in ans:
-            print(point[0], point[1])
-        print("~")
-
-# } Driver Code Ends
+    def kClosest(self, points, k):
+        # code here
+        import heapq
+        pq=[]
+        for x,y in points:
+            heapq.heappush(pq,(-x*x-y*y,x,y))
+            if len(pq)>k:
+                heapq.heappop(pq)
+        return [(x,y) for _,x,y in pq]
