f3812371d579812d308f5f0d604a8a5aeb19c5a0
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-01-04 20:35:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -1,5 +1,7 @@
 class Solution:
     def sort012(self, arr):
         # code here
-        arr.sort()
-        
+        z=arr.count(0)
+        o=arr.count(1)
+        t=arr.count(2)
+        arr[:]=[0]*z+[1]*o+[2]*t
