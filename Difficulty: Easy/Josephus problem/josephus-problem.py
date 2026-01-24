23458f3ae8f0995473ff468a81c241eb27e54cab
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-01-24 22:05:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Josephus problem/josephus-problem.py
@@ -0,0 +1,12 @@
+class Solution:
+    def josephus(self, n, k):
+        # code here
+        nxt=[*range(1,n)]+[0]
+        prv=[n-1]+[*range(n-1)]
+        cur=-1
+        while nxt[cur]!=cur:
+            for _ in range(k):
+                cur=nxt[cur]
+            nxt[prv[cur]]=nxt[cur]
+            prv[nxt[cur]]=prv[cur]
+        return cur+1
