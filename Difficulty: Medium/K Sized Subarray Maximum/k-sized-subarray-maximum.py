4d951f37566317eca2a9cafc0ae6d99e39f1ddf1
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-02-03 21:17:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
@@ -13,38 +13,3 @@ class Solution:
             if i >= k-1:
                 ans.append(arr[q[0]])
         return ans
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-from collections import deque
-
-#Contributed by : Nagendra Jha
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        arr = list(map(int, input().strip().split()))
-        k = int(input())
-        ob = Solution()
-        res = ob.maxOfSubarrays(arr, k)
-        for i in range(len(res)):
-            print(res[i], end=" ")
-        print()
-        print("~")
-# } Driver Code Ends
