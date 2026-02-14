ae96bd16adcb84feeea1f237cbcc66b474798086
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-02-15 01:55:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kokoEat(self, arr, k):
+        # Code here
+        import numpy as np
+        arr = np.array(arr)
+        low, high = 1, arr.max()
+        ans = high
+        while low <= high:
+            mid = (low + high) // 2
+            hours = np.sum(np.ceil(arr / mid))
+            if hours <= k:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+
