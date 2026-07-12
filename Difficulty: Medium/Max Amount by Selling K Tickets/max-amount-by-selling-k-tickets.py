ee6165088e55d65c264dda72e6cafaf72b680b53
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-07-12 23:07:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Amount by Selling K Tickets/max-amount-by-selling-k-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxAmount(self, arr, k):
+        # code here
+        import heapq
+        mod = 10**9+7
+        heap = [-x for x in arr]
+        heapq.heapify(heap)
+        ans = 0
+        while k>0 and heap:
+            cur = -heapq.heappop(heap)
+            ans = (ans+cur)%mod
+            if cur > 1:
+                heapq.heappush(heap, -(cur-1))
+            k-=1
+        return ans
